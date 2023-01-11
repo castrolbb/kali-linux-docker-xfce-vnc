02828bc79bda4c5e93214d9b12a107d18e9d5911
castrolbb <castrolbb@gmail.com>
2023-01-10 23:42:14 -0300

    Delete getting-started-with-docker-compose directory

--- getting-started-with-docker-compose/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import sys
-import os
-
-m1 = os.environ["M1"]
-print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", str(sys.argv), m1, os.listdir("/data/"))
